11418bf81157b5347437d52c31c680a59ed709ca
Nico Moreno <nicom55@gmail.com>
2015-02-25 13:23:35 +0000

    added actions to goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,27 +5,44 @@ class GoalsController < ApplicationController
   end
   
   def show
-
+    @goal = Goal.find(params[:id])
   end
 
   def new
-
   end
 
   def edit
-
+    @goal = Goal.find(params[:id])
   end
 
   def create
-
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to goals_path
+    else
+      @errors = @goal.errors
+      render :new
+    end
   end
 
   def update
-
+    @goal = Goal.find(params[:id])
+    if @goal.update(goal_params)
+      redirect_to goals_path
+    else
+      @errors = @goal.errors
+      render :edit
   end
 
   def delete
-
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to goals_path
   end
+    
+    private 
+    def goal_params
+      params.require(:goal).permit(:title, :body)
+    end
   
 end
