cfa6a167b17d579820baa836d889b00b2e973e66
Nico Moreno <nicom55@gmail.com>
2015-02-25 12:46:27 +0000

    added goals actions and views

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,4 +4,28 @@ class GoalsController < ApplicationController
     @goals = Goal.all
   end
   
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+  
 end
