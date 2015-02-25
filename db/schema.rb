53c5e8e7b0889f776450ac79e6c44e647cdcf54d
Nico Moreno <nicom55@gmail.com>
2015-02-25 02:54:52 +0000

    added User validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224125230) do
+ActiveRecord::Schema.define(version: 20150224133017) do
 
   create_table "goals", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first"
+    t.string   "last"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
