9bd3f5ffa38461065519576c0c4fb8994ea0093f
Nico Moreno <nicom55@gmail.com>
2015-03-03 12:22:52 +0000

    added comments controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,20 @@ class CommentsController < ApplicationController
     redirect_to :back
   end
   
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+  
+  def delete
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to :back
+  end
+  
   private
   def comment_params
     params.require(:comment).permit(:user_id, :goal_id, :body)
