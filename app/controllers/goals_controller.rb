54e5e2f73b85011e2e10bf33ad8b1340e49590b8
Nico Moreno <nicom55@gmail.com>
2015-03-06 23:31:37 +0000

    worked on comment destroy action

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -6,9 +6,12 @@ class GoalsController < ApplicationController
   
   def show
     @goal = Goal.find(params[:id])
-    #trying to fix the .comments error
-   # @comment = @goal.comments.create(params[:comment].permit(:body))
-   # redirect_to goal_path(@goal)
+    
+    #the below line creates a new "delete comment" link every time you reload the page
+    #@comment = @goal.comments.create(params[:comment])
+    
+    #the below line causes a redirect loop
+    #redirect_to goal_path(@goal)
   end
 
   def new
