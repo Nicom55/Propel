e6e1951666981620f9b5ff2bb4ac6339ab493a58
Nico Moreno <nicom55@gmail.com>
2015-03-10 12:36:52 +0000

    added comment tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # i know these need to be modified
+  test "should create new comment" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should show comment" do
+    get :show
+    assert_response :success
+  end
+  
+  test "should get new comment" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should destroy comment" do
+    get :destroy
+    assert_response :success
+  end
+  
 end
