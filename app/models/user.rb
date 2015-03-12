2de32674995808ff79c3d7601fba80079262721d
Nico Moreno <nicom55@gmail.com>
2015-03-12 13:16:51 +0000

    add encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :password
+  before_save :encrypt_password
   
   # linking users to goals
   has_many :goals
@@ -16,5 +17,12 @@ class User < ActiveRecord::Base
   validates :first, presence: true
   validates :last, presence: true
   validates :email, presence: true
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
   
 end
