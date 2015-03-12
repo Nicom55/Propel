531825d5b0f588597a65cc81955ecebb5bf825b4
Nico Moreno <nicom55@gmail.com>
2015-03-12 13:05:34 +0000

    add new user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
   
   # linking users to goals
   has_many :goals
   has_many :likes
   has_many :comments
   
-  #validations
+  #validations for user signup
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  
+  #other validations
   validates :first, presence: true
   validates :last, presence: true
   validates :email, presence: true
