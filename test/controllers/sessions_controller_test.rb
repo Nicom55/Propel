55f89a03338d7de30d4d17377c329f09e544d2f5
Nico Moreno <nicom55@gmail.com>
2015-03-17 12:13:27 +0000

    add sessions tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
+  
+  test "should create a new login session" do
     get :new
     assert_response :success
+  end
+  
+  test "should end a user's login session" do
+    assert_routing({ path: 'sessions', method: :delete }, { controller: 'sessions', action: 'destroy' })
   end
 
 end
