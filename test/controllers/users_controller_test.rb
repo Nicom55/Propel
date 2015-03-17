1fdc1e7d69c3eca114839d8746854191644cc597
Nico Moreno <nicom55@gmail.com>
2015-03-17 12:20:08 +0000

    add invalid user test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,11 +3,19 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   
   def setup
-    @user = User.new(first: "Bob", last: "Jones", email:"test@gmail.com")
+    @user = User.new(first: "Bob", last: "Jones", email: "bobbyj@gmail.com", password: "cl3verpassw0rd" )
   end
   
-  test "should be valid" do
+  test "user should be valid" do
     assert @user.valid?
+  end
+  
+  def setup
+    @bad_user = User.new(first: "Rob", email: "robbyj@gmail.com", password: "nolastname" )
+  end
+  
+  test "user should not pass validations" do
+    assert @bad_user.valid?
   end
   
 end
